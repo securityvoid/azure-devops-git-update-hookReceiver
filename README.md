38c3948a0f2cd18638eccc23ee55628c4f889ac0
Douglas Logan <dlogan@cyberninjas.com>
2020-05-07 10:21:19 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # azure-devops-git-update-hookReceiver
 A light-weight HTTP Listener set to receive a Personal Access Token or similar, and utilize that token to pull the latest updates from a git repository.
+
+## Installation
+1. Clone this repository
+2. Create a directory "ssl" in the base of your cloned project and generate your SSL Certificates with the following openssl commands:
+`openssl genrsa -out key.pem
+openssl req -new -key key.pem -out csr.pem
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
+rm csr.pem`
+3. Set the following environmental variables or put them in a .env file in the base of the directory:
+**HTTP_LISTENER_PORT** The port that the HTTP Listener should listen on.
+**HASHED_AUTHORIZATION_BEARER** The expected Authorization Bearer value run through the function getPwdHashFromPwd.
+**TARGET_GIT_DIRECTORY** The location of the git directory that you want to run git pull on.
+**SSL_PRIVATE_KEY** The name for the SSL private key. This should probably be key.pem .
+**SSL_PUBLIC_KEY**  The name for the SSL public key. This should probably be cert.pem .
+3. Run the script with the following command:
+`node index.js`
+
+## HTTP Request Format
+HTTP Requests should be in the following format:
+`POST / HTTP/1.1
+Host: [HOSTNAME]
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+authorization: Bearer [AUTHORIZATION_BEARER_VALUE]
+Connection: close
+Content-Length: 67
+
+access_token=[ACCESS_TOKEN_VALUE]`
+
+Where [AUTHORIZATION_BEARER_VALUE] is the authentication token that you've run through getPwdHashFromPwd and stored its value into the environmental variable HASHED_AUTHORIZATION_BEARER
+and [ACCESS_TOKEN_VALUE] is the personal access token or similar from Azure DevOps that is allowed to pull the repository. [HOSTNAME] must also be filled out.
+
+### Example HTTP Request
+The following is an example HTTP Request:
+`
+POST / HTTP/1.1
+Host: localhost
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+authorization: Bearer KgUGw2euwyL5p6UADMAYd4f0QG-Mjmc1HHyLycaAScg9uUa2SZPLSqu0IokjEfRD
+Connection: close
+Content-Length: 67
+
+access_token=esqag7hurb6zeqfrenz6o4f2omq4c24jvgcenbudgneevqwzszda
+
+`
+In the above example, the value of HASHED_AUTHORIZATION_BEARER should be "o401q6Z+vmL7I/p3EUbuG57mYHB1MSPsZiJK56+EM+o=".
+
+The HTTP Response will either be a 401 Unauthorized if ANY type of error happens or fails to validate, or an HTTP 200 because it was successful.
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,101 @@
+const fs        = require('fs');
+const crypto    = require('crypto');
+const path      = require('path');
+const q         = require('q');
+const git       = require('simple-git/promise');
+require('dotenv').config();
+
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.pem'))
+};
+
+require('https').createServer(options, function(req, res) {
+    let data = '';
+    if(!req.headers || !req.headers.authorization || !validateToken(req.headers.authorization.replace(/^Bearer\s+/,'')) )
+        sendResponse(res, 401,{ 'error': 'Access Denied'});
+    else {
+        if (req.method == "POST") {
+            req.on('data', function(chunk) {
+                data += chunk;
+            });
+
+            req.on('end', function() {
+                if(/^access_token=([^&=\s\n\r]+)\r\n$/.test(data))
+                    gitPull(data.replace(/^access_token=/,'').trim()).then(function(){
+                        sendResponse(res, 200,{ 'Success': 'true'});
+                    }).catch(function(){
+                        sendResponse(res, 401,{ 'error': 'Access Denied'});
+                    });
+                else
+                    sendResponse(res, 401,{ 'error': 'Access Denied'});
+            });
+        } else {
+            sendResponse(res, 401,{ 'error': 'Access Denied'});
+        }
+    }
+}).listen(process.env.HTTP_LISTENER_PORT);;
+
+
+/**
+ * sendResponse - Sends the Response Back to the Browser
+ * @param res - Response Object from http
+ * @param statusCode - The StatusCode to return on the response
+ * @param body - The body to returned. Needs to be in a JSON format.
+ */
+function sendResponse(res, statusCode, body){
+    if(typeof body != 'string')
+        body = JSON.stringify(body);
+    res.writeHead(statusCode, {'Content-Length': Buffer.byteLength(body), 'Content-Type': 'application/json'}).end(body);
+    res.end();
+}
+
+/**
+ * validateToken - Takes a token value, hashes it through getPwdHashFromPwd and sees if it matches the ENV variable password.
+ * @param tokenValue - The token value supplied on the HTTP Request
+ * @returns {boolean} - Returns true if it matches, false if it does not.
+ */
+function validateToken(tokenValue){
+    if(getPwdHashFromPwd(tokenValue) == process.env.HASHED_AUTHORIZATION_BEARER)
+        return true;
+    else
+        return false;
+}
+
+/**
+ * getPwdHashFromPwd - Takes the password and hashes it 250 times to returned the hashed value and add time / weight.
+ * @param pwd - The password to be hashed.
+ * @returns - base64 encoded string with the final hash value.
+ */
+function getPwdHashFromPwd(pwd){
+    let hash = pwd;
+    for(let x=0; x < 250; x++){
+        hash = crypto.createHash('sha256').update(hash).digest('base64');
+    }
+    return hash;
+}
+
+/**
+ * gitPull - Performs the gitPull on the repository specified in the ENV variable TARGET_GIT_DIRECTORY.
+ * @param tokenValue - The token to utilize when doing the git pull.
+ * @returns {*|promise|Promise<any>} -  Resolve (Results) - The Pull executed properly
+ *                                      Reject (Error) - The Pull failed.
+ */
+function gitPull(tokenValue){
+    let deferred = q.defer();
+    git(process.env.TARGET_GIT_DIRECTORY).getRemotes(true).then(function(remotes){
+        const pushURL= remotes[0].refs.push;
+        const credPushUrl = pushURL.slice(0, 8) + "batchjob:" + encodeURIComponent(tokenValue) + '@' + pushURL.slice(8);
+        git(process.env.TARGET_GIT_DIRECTORY).env('GCM_INTERACTIVE', 'never').env('GIT_TERMINAL_PROMPT', '0').pull(credPushUrl).then(function(response){
+            deferred.resolve(response);
+        }).catch(function(error){
+            deferred.reject(error);
+        });
+    }).catch(function(error){
+        deferred.reject(error);
+    });
+    return deferred.promise;
+}
+
+//Outputs the listening port so its clear where / how its listening.
+console.log("Listening to port " + process.env.HTTP_LISTENER_PORT);
